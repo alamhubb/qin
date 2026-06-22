66218e4b3e8d1a155fe82cd5688d07f5a547e3f4
alamhubb <alamhubb@163.com>
2026-06-23 01:43:02 +0800

    Move balance monitor to standalone repo

--- examples/balance-monitor-system/app/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from "vue"
-import BalanceDashboard from "./AdminDashboard.ovs"
-import { BalancePanel } from "./BalancePanel.js"
-import "./style.css"
-
-createApp(BalanceDashboard).mount("#app")
-createApp(BalancePanel).mount("#balance-panel")
